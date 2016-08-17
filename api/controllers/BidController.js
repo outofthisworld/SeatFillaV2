d2bf2193e9d2aa288f6cea882fd1d12609f85b79
Dale <outofthisworld24@hotmail.com>
2016-08-17 19:20:05 +1200

    updated request model

---
+++ api/controllers/BidController.js
@@ -0,0 +1,11 @@
+/**
+ * BidController
+ *
+ * @description :: Server-side logic for managing Bids
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

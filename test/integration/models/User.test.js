9720f6d7d808fd3538a6d8dc3c8c604bb64222e9
Dale <outofthisworld24@hotmail.com>
2016-08-26 13:28:38 +1200

    saved comments

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,13 +1,18 @@
 
 
 
-
-
+//Descibe takes a string and a function that is called back and executed.
+//The string defines what the tests do, and can be nested inside one another.
 describe('User tests',function(){
+
+    //This is our second level of nesting for descibe functions, it descibes what our tests do
+    //in more detail.
     describe('Create users',function(){
+        //Remember to call the done function always.
         it('Creates a user', function(done){
 
-        User.create({
+        //Creates a new user
+        User.create({ //Check model files for corresponding object properties
             id: 1234,
             username: 'something',
             password: 'testpassword',
@@ -23,16 +28,22 @@ describe('User tests',function(){
             isLockedOut: false,
             isEmailVerified: false,
             ip: '127.0.0.1'
-          }).exec(function(err,createdUser){
+                        
+          }).exec(function(err,createdUser){   //Callback function returns error and the created user.
 
-              if(err){
-                  return done(err);
+              if(err){ //Checks for error
+                  return done(err); //Calls the done function with the error (test failed)
               }else{
                   sails.log.debug('Succesfully created a new user');
-                  return done();
+                  return done(); //Test passed (no arguments needed)
               }
               
           });
+        });
+
+        //How to define another method within descibe
+        it('creates user method 2', function(done){
+            return done();
         });
     })
 });

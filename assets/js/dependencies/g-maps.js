2c38419769cfc21748d69964d6fa4bd67573e504
Dale <outofthisworld24@hotmail.com>
2016-09-16 15:32:07 +1200

    upd

--- assets/js/dependencies/g-maps.js
+++ assets/js/dependencies/g-maps.js
@@ -19,89 +19,37 @@
 
 var _seat_filla_map = function(options) {
 
-    var x = {
-        map: null,
-        location: (function(options) {
-            geolocator.config({ language: 'en', google: { version: '3', key: 'AIzaSyDDBWrH7DuCZ8wNlOXgINCtI_gT9NkDRq4' } });
-            defaultLoc = { coords: { longitude: 0, latitude: 0 } };
-            if (options && options.useCache && typeof Storage !== "undefined" && sessionStorage.getItem('location')) {
-                var location = JSON.parse(sessionStorage.getItem('location'));
-                return location;
-            } else {
-                console.log('Could not find location in session storage');
-                geolocator.locate(options, function(err, location) {
-                    if (err) {
-                        if (!navigator.geolocation) {
-                            console.log('Error when locating position ' + err + '. Defaulting coordinates');
-                            return defaultLoc;
-                        } else {
-                            navigator.geolocation.getCurrentPosition(function(position) {
-                                return position;
+    var _instance = {};
+    _instance.map = null;
+    _instance.addMarker = function(options) {
 
-                            }, function error() {
-                                console.log('Error when using navigator geolocation');
-                                return defaultLoc;
-                            });
-                        }
-                    } else {
-                        console.log('Creating map, used geolocator (uncached) to retrieve location ' + location);
-                        if (options && options.useCache && typeof Storage !== "undefined" && location) {
-                            sessionStorage.setItem('location', JSON.stringify(location));
-                        }
-                        return location;
-                    }
-                });
-            }
-        })(options),
+        if (!options) {
+            throw new Error('Invalid args passed to addMarker (_seat_filla_map)');
+        }
 
-        configure: function(options) {
-            const _map = this;
-            _map.map = (function(location) {
-                var pos = {
-                    lat: location.coords.latitude,
-                    lng: location.coords.longitude
-                };
-                //Create a new map
-                var map = new google.maps.Map(document.getElementById('map-canvas'), {
-                    zoom: (options && options.zoom) || 2,
-                    center: (options && options.coords) || pos
-                });
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var map = this.map;
 
-                google.maps.event.addListener(map, 'click', function(event) {
-                    _map.addMarker(event.latLng, 'Some title', 'Airport', 'Content');
-                });
+        var marker = new google.maps.Marker(options);
 
-                var infoWindow = new google.maps.InfoWindow({ map: map });
-                map.setCenter(pos);
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Your location');
-                return map;
-            })(options || this.location || this.geoLocate());
-        },
-
-        addMarker: function(location, title, markerSymbol, contentInfo) {
-            // Add the marker at the clicked location, and add the next-available label
-            // from the array of alphabetical characters.
-            var map = this.map;
-
-            var marker = new google.maps.Marker({
-                position: location,
-                label: markerSymbol,
-                title: title,
-                map: map,
-                animation: google.maps.Animation.DROP,
-                draggable: true,
-
-            });
-
+        if (options.content) {
             marker.addListener('click', function() {
                 var infowindow = new google.maps.InfoWindow({
-                    content: contentInfo
+                    content: options.content
                 });
+
+                marker.infowindow = {
+                    window: infowindow,
+                    open: true
+                }
+
                 infowindow.open(map, marker);
             });
+        }
 
-            marker.addListener(marker, 'click', function(event) {
+        if (options.markerClickAnimation) {
+            marker.addListener('click', function(event) {
                 if (marker.getAnimation() !== null) {
                     marker.setAnimation(null);
                 } else {
@@ -109,6 +57,128 @@ var _seat_filla_map = function(options) {
                 }
             });
         }
+
+        if (options.onClickListeners) {
+            if (Array.isArray(options.onClickListeners.markerListeners)) {
+                options.onClickListeners.markerListeners.forEach((element) => {
+                    marker.addListener('click', element.bind(marker));
+                })
+            }
+
+            if (Array.isArray(options.onClickListeners.mapListeners)) {
+                options.onClickListeners.mapListeners.forEach((element) => {
+                    map.addListener('click', element.bind(marker));
+                })
+            }
+        }
+
+        return marker;
     }
-    return x;
+
+    function createMap(loc) {
+        _instance.location = loc;
+
+        var pos = {
+            lat: _instance.location.coords.latitude,
+            lng: _instance.location.coords.longitude
+        };
+        //Create a new map
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+            zoom: (options && options.zoom) || 2,
+            center: (options && options.coords) || pos
+        });
+
+        google.maps.event.addListener(map, 'click', function(event) {
+            _instance.addMarker({
+                position: event.latLng,
+                title: 'Some title',
+                map: map,
+                animation: google.maps.Animation.DROP,
+                markerClickAnimation: google.maps.Animation.BOUNCE,
+                draggable: true,
+                icon: {
+                    url: 'http://127.0.0.1:1337/images/g_maps_airport_icon.png',
+                    // This marker is 20 pixels wide by 32 pixels high.
+                    size: new google.maps.Size(32, 32),
+                    // The origin for this image is (0, 0).
+                    origin: new google.maps.Point(0, 0),
+                    // The anchor for this image is the base of the airport icon at (0, 32).
+                    anchor: new google.maps.Point(32 / 2, 32)
+                },
+
+                onClickListeners: {
+                    mapListeners: [],
+                    markerListeners: [
+                        function onMarkerClicked(event) {
+                            var result = geolocator.calcDistance({
+                                from: _instance.location.coords,
+                                to: {
+                                    latitude: event.latLng.lat(),
+                                    longitude: event.latLng.lng()
+                                },
+                                formula: geolocator.DistanceFormula.HAVERSINE,
+                                unitSystem: geolocator.UnitSystem.METRIC
+                            });
+                        }
+                    ]
+                },
+                content: '<h1>Its an airport!</h1>'
+            });
+        });
+
+        (function setUserPosition() {
+            var infoWindow = new google.maps.InfoWindow({ map: map });
+            map.setCenter(pos);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Your location');
+            _instance.addMarker({
+                position: pos,
+                title: 'You',
+                label: 'Y',
+                map: map,
+                content: 'Your position',
+                animation: google.maps.Animation.DROP,
+                markerClickAnimation: google.maps.Animation.BOUNCE,
+                draggable: true,
+            });
+        })();
+
+        _instance.map = map;
+    }
+
+
+    (function init(cb) {
+        geolocator.config({ language: 'en', google: { version: '3', key: 'AIzaSyDDBWrH7DuCZ8wNlOXgINCtI_gT9NkDRq4' } });
+        defaultLoc = { coords: { longitude: 0, latitude: 0 } };
+        if (options && options.useCache && typeof Storage !== "undefined" && sessionStorage.getItem('location')) {
+            var location = JSON.parse(sessionStorage.getItem('location'));
+            cb(location);
+        } else {
+            console.log('Could not find location in session storage');
+            geolocator.locate(options, function(err, location) {
+                if (err) {
+                    if (!navigator.geolocation) {
+                        console.log('Error when locating position ' + err + '. Defaulting coordinates');
+                        cb(defaultLoc);
+                    } else {
+                        navigator.geolocation.getCurrentPosition(function(position) {
+                            cb(position);
+
+                        }, function error() {
+                            console.log('Error when using navigator geolocation');
+                            cb(defaultLoc);
+                        });
+                    }
+                } else {
+                    console.log('Creating map, used geolocator (uncached) to retrieve location ' + location);
+                    if (options && options.useCache && typeof Storage !== "undefined" && location) {
+                        sessionStorage.setItem('location', JSON.stringify(location));
+                    }
+                    cb(location);
+                }
+            });
+        }
+    })(createMap);
+
+    return _instance;
 }

11c3ff47e76df1e92a0e5fdc71a09d53c9e862d4
Dale <outofthisworld24@hotmail.com>
2016-09-16 15:59:10 +1200

    cm

--- assets/js/dependencies/g-maps.js
+++ assets/js/dependencies/g-maps.js
@@ -1,4 +1,7 @@
 /*
+
+    Created by Dale.
+    
     Contains the logic for integrating the site with googlemaps
     and the javascript geolocation API.
 
@@ -21,7 +24,11 @@ var _seat_filla_map = function(options) {
 
     var _instance = {};
     _instance.map = null;
-    _instance.addMarker = function(options) {
+
+
+    const executableQueue = [];
+
+    _instance.addMarker = function(options, cb) {
 
         if (!options) {
             throw new Error('Invalid args passed to addMarker (_seat_filla_map)');
@@ -29,50 +36,63 @@ var _seat_filla_map = function(options) {
 
         // Add the marker at the clicked location, and add the next-available label
         // from the array of alphabetical characters.
-        var map = this.map;
+        var map = _instance.map;
 
-        var marker = new google.maps.Marker(options);
 
-        if (options.content) {
-            marker.addListener('click', function() {
-                var infowindow = new google.maps.InfoWindow({
-                    content: options.content
+        function add() {
+            var marker = new google.maps.Marker(options);
+
+            if (options.content) {
+                marker.addListener('click', function() {
+                    var infowindow = new google.maps.InfoWindow({
+                        content: options.content
+                    });
+
+                    marker.infowindow = {
+                        window: infowindow,
+                        open: true
+                    }
+
+                    infowindow.open(map, marker);
                 });
-
-                marker.infowindow = {
-                    window: infowindow,
-                    open: true
-                }
-
-                infowindow.open(map, marker);
-            });
-        }
-
-        if (options.markerClickAnimation) {
-            marker.addListener('click', function(event) {
-                if (marker.getAnimation() !== null) {
-                    marker.setAnimation(null);
-                } else {
-                    marker.setAnimation(google.maps.Animation.BOUNCE);
-                }
-            });
-        }
-
-        if (options.onClickListeners) {
-            if (Array.isArray(options.onClickListeners.markerListeners)) {
-                options.onClickListeners.markerListeners.forEach((element) => {
-                    marker.addListener('click', element.bind(marker));
-                })
             }
 
-            if (Array.isArray(options.onClickListeners.mapListeners)) {
-                options.onClickListeners.mapListeners.forEach((element) => {
-                    map.addListener('click', element.bind(marker));
-                })
+            if (options.markerClickAnimation) {
+                marker.addListener('click', function(event) {
+                    if (marker.getAnimation() !== null) {
+                        marker.setAnimation(null);
+                    } else {
+                        marker.setAnimation(google.maps.Animation.BOUNCE);
+                    }
+                });
             }
+
+            if (options.onClickListeners) {
+                if (Array.isArray(options.onClickListeners.markerListeners)) {
+                    options.onClickListeners.markerListeners.forEach((element) => {
+                        marker.addListener('click', element.bind(marker));
+                    })
+                }
+
+                if (Array.isArray(options.onClickListeners.mapListeners)) {
+                    options.onClickListeners.mapListeners.forEach((element) => {
+                        map.addListener('click', element.bind(marker));
+                    })
+                }
+            }
+
+            return marker;
         }
 
-        return marker;
+        if (map) {
+            executableQueue.forEach((exec) => {
+                exec.cb.call(this, exec.options);
+            });
+            add();
+        } else {
+            executableQueue.push({ cb: add, options: options });
+        }
+
     }
 
     function createMap(loc) {
@@ -87,6 +107,23 @@ var _seat_filla_map = function(options) {
             zoom: (options && options.zoom) || 2,
             center: (options && options.coords) || pos
         });
+
+        (function setUserPosition() {
+            var infoWindow = new google.maps.InfoWindow({ map: map });
+            map.setCenter(pos);
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Your location');
+            _instance.addMarker({
+                position: pos,
+                title: 'You',
+                label: 'Y',
+                map: map,
+                content: 'Your position',
+                animation: google.maps.Animation.DROP,
+                markerClickAnimation: google.maps.Animation.BOUNCE,
+                draggable: true,
+            });
+        })();
 
         google.maps.event.addListener(map, 'click', function(event) {
             _instance.addMarker({
@@ -110,38 +147,30 @@ var _seat_filla_map = function(options) {
                     mapListeners: [],
                     markerListeners: [
                         function onMarkerClicked(event) {
+                            console.log({
+                                latitude: _instance.location.coords.latitude,
+                                longitude: _instance.location.coords.longitude
+                            });
+                            console.log(event.latLng.lat());
+                            console.log(event.latLng.lng());
                             var result = geolocator.calcDistance({
-                                from: _instance.location.coords,
+                                from: {
+                                    latitude: _instance.location.coords.latitude,
+                                    longitude: _instance.location.coords.longitude
+                                },
                                 to: {
                                     latitude: event.latLng.lat(),
                                     longitude: event.latLng.lng()
                                 },
-                                formula: geolocator.DistanceFormula.HAVERSINE,
-                                unitSystem: geolocator.UnitSystem.METRIC
+
                             });
+                            console.log(result);
                         }
                     ]
                 },
                 content: '<h1>Its an airport!</h1>'
             });
         });
-
-        (function setUserPosition() {
-            var infoWindow = new google.maps.InfoWindow({ map: map });
-            map.setCenter(pos);
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Your location');
-            _instance.addMarker({
-                position: pos,
-                title: 'You',
-                label: 'Y',
-                map: map,
-                content: 'Your position',
-                animation: google.maps.Animation.DROP,
-                markerClickAnimation: google.maps.Animation.BOUNCE,
-                draggable: true,
-            });
-        })();
 
         _instance.map = map;
     }

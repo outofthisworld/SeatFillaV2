af1553a4282b99269f421398ec44f3944b0a029c
Dale <outofthisworld24@hotmail.com>
2016-09-21 21:57:56 +1200

    upd

--- config/misc.js
+++ config/misc.js
@@ -1,9 +1,14 @@
+/*
+
+For production
+
+
 const fs = require('fs');
 
-modules.exports = {
+module.exports = {
     port: 443,
     ssl: {
-        key: fs.readFileSync('seatfilla.key'),
-        cert: fs.readFileSync('seatfilla.crt')
+        key: fs.readFileSync(process.cwd() + '/seatfilla.key'),
+        cert: fs.readFileSync(process.cwd() + '/1_Intermediate.crt'),
     }
-}
+}*/
